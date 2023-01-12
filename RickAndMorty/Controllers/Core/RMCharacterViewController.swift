48e25fc889f2930631fbeea784197bce7a868c3a
ToshiroSama <alishdjuraev@gmail.com>
2023-01-12 16:17:41 +0500

    Character List View is added. UICollectionView

--- RickAndMorty/Controllers/Core/RMCharacterViewController.swift
+++ RickAndMorty/Controllers/Core/RMCharacterViewController.swift
@@ -6,15 +6,29 @@
 //
 
 import UIKit
+import SnapKit
 
 /// Controller to show and search for Characters
 final class RMCharacterViewController: UIViewController {
+    
+    private let characterListView = RMCharacterListView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .systemBackground
-        title = "Characters"
+        self.view.backgroundColor = .systemBackground
+        self.title = "Characters"
+        
+        self.setupControllerUI()
+    }
+    
+    private func setupControllerUI() {
+        self.view.addSubview(characterListView)
+        characterListView.snp.makeConstraints { make in
+            make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top)
+            make.leading.equalTo(self.view.safeAreaLayoutGuide.snp.leading)
+            make.trailing.equalTo(self.view.safeAreaLayoutGuide.snp.trailing)
+            make.bottom.equalTo(self.view.safeAreaLayoutGuide.snp.bottom)
+        }
     }
    
 }
---
+++ RickAndMorty/Views/RMCharacterListView.swift
@@ -0,0 +1,79 @@
+//
+//  RMCharacterListView.swift
+//  RickAndMorty
+//
+//  Created by Alisher Djuraev on 12/01/23.
+//
+
+import UIKit
+import SnapKit
+
+/// View that handles showing list of characters, loader, etc.
+final class RMCharacterListView: UIView {
+    
+    private let viewModel = RMCharacterListViewViewModel()
+    
+    private let spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView(style: .large)
+        spinner.hidesWhenStopped = true
+        return spinner
+    }()
+    
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.isHidden = true
+        collectionView.alpha = 0
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
+        return collectionView
+    }()
+    
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.setupUI()
+        
+        spinner.startAnimating()
+        viewModel.fetchCharacters()
+        
+        setupCollectionView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+        self.addSubviews(collectionView, spinner)
+        spinner.snp.makeConstraints { make in
+            make.size.equalTo(CGSize(width: 100, height: 100))
+            make.centerX.centerY.equalToSuperview()
+        }
+        
+        collectionView.snp.makeConstraints { make in
+            make.top.equalTo(self.snp.top)
+            make.leading.equalTo(self.snp.leading)
+            make.trailing.equalTo(self.snp.trailing)
+            make.bottom.equalTo(self.snp.bottom)
+        }
+    }
+    
+    private func setupCollectionView() {
+        collectionView.dataSource = viewModel
+        collectionView.delegate = viewModel
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2, execute: {
+            self.spinner.stopAnimating()
+            
+            self.collectionView.isHidden = false
+            
+            UIView.animate(withDuration: 0.4) {
+                self.collectionView.alpha = 1.0
+            }
+        })
+    }
+}
